2154c79422984c055e65ca0dd2a34a840a591c6b
troy-ameigh <ameighta@amazon.com>
2020-04-14 09:41:06 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'quickstart-check = quickstart_pre_commit_hook.pre_commit_hook:quickstart_check',
+            'quickstart_check = quickstart_pre_commit_hook.pre_commit_hook:quickstart_check',
         ],
     },
 
