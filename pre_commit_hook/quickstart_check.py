e350ba6ea4ba2bd147df1d13355b625e9847dcda
troy-ameigh <ameighta@amazon.com>
2020-04-14 09:35:27 -0500

    made executible

---
+++ pre_commit_hook/quickstart_check.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+def extensioncheck():
+ if os.path.exists('./test/*.template.yaml'):
+    print ("File Naming Standard Met = PASS")
+ else:
+    print ("Please Rename Following files to QuickStart Standard of *.template.yaml")
+    arr = os.listdir('test')
+    print(arr)
+
+extensioncheck()
